70529d8fb3e95444b029b0692f62f39b781ce3ee
Will Roberts <William.Roberts@dfki.de>
2010-08-26 06:54:42 +0800

    initial check-in of dashboard/api
    still need to implement authentication
    still need to check json/xml/yaml access works (but it should)

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,7 @@
 Project: MT Server Land prototype code
  Author: Christian Federmann <cfedermann@dfki.de>
 """
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import *
 
 urlpatterns = patterns('serverland.dashboard.views',
   (r'^$', 'dashboard'),
@@ -10,4 +10,5 @@ urlpatterns = patterns('serverland.dashboard.views',
   (r'^delete/(?P<request_id>[a-f0-9]{32})/$', 'delete'),
   (r'^result/(?P<request_id>[a-f0-9]{32})/$', 'result'),
   (r'^download/(?P<request_id>[a-f0-9]{32})/$', 'download'),
+  (r'^api/', include('serverland.dashboard.api.urls')),
 )
