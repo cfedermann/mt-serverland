1c91db9731a875b2a84fa1dfb172ca0b662bb7b6
Christian Federmann <cfedermann@gmail.com>
2010-09-16 10:11:57 +0200

    Fixed problems with non named fields in format calls.

--- dashboard/api/xmlrpcserver.py
+++ dashboard/api/xmlrpcserver.py
@@ -15,7 +15,13 @@ dashboard_api_url -- the URL of the serverland dashboard API
 '''
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+
+# Question for Will: do we really need httplib2?
+# - http://code.google.com/p/httplib2/
+#
+# Or would the httplib import suffice?
 import httplib2
+
 import json
 import mimetools
 import sys
@@ -34,7 +40,7 @@ def normalize_api_url (url):
     if not url_path.endswith('/'):
         url_path += '/'
     url_path_parts = [ x for x in url_path.split('/') if x ]
-    if url_path_parts[-2:] == ['dashboard','api']:
+    if url_path_parts[-2:] == ['dashboard', 'api']:
         pass
     elif url_path_parts[-1:] == ['dashboard']:
         url_path += 'api/'
@@ -74,7 +80,7 @@ class XmlRpcAPIServer(object):
         Starts the event handler of the XML-RPC API interface server.
         '''
         print 'Starting server'
-        print 'Dashboard API lives at {}'.format(self.api_url)
+        print 'Dashboard API lives at {0}'.format(self.api_url)
         while not self.finished:
             self.server.handle_request()
 
@@ -90,9 +96,10 @@ class XmlRpcAPIServer(object):
         Queries the server for workers.
         '''
         if shortname is not None:
-            url = self.api_url + 'workers/{}/?token={}'.format(shortname, token)
+            url = self.api_url + 'workers/{0}/?token={1}'.format(shortname,
+              token)
         else:
-            url = self.api_url + 'workers/?token={}'.format(token)
+            url = self.api_url + 'workers/?token={0}'.format(token)
         response = self.http.request(url, method='GET')
         if response[0].status == 200:
             return json.loads(response[1])
@@ -104,10 +111,10 @@ class XmlRpcAPIServer(object):
         Queries the server for translation requests.
         '''
         if shortname_or_request_id is not None:
-            url = self.api_url + 'requests/{}/?token={}'.format(
-                shortname_or_request_id, token)
+            url = self.api_url + 'requests/{0}/?token={1}'.format(
+              shortname_or_request_id, token)
         else:
-            url = self.api_url + 'requests/?token={}'.format(token)
+            url = self.api_url + 'requests/?token={0}'.format(token)
         response = self.http.request(url, method='GET')
         if response[0].status == 200:
             return json.loads(response[1])
@@ -133,20 +140,21 @@ class XmlRpcAPIServer(object):
         body = []
         for (key, value) in contents.items():
             body.append('--' + boundary)
-            body.append('Content-Disposition: form-data; name="{}"'.format(key))
+            body.append('Content-Disposition: form-data; name="{0}"'.format(
+              key))
             body.append('')
             body.append(value)
         body.append('--' + boundary)
         body.append('Content-Disposition: form-data; ' +
                       'name="source_text"; ' +
-                      'filename="{}"'.format(file_name))
+                      'filename="{0}"'.format(file_name))
         body.append('Content-Type: text/plain')
         body.append('')
         body.extend(file_lines)
         body.append('--' + boundary)
         body.append('')
         body = CRLF.join(body)
-        content_type = 'multipart/form-data; boundary={}'.format(boundary)
+        content_type = 'multipart/form-data; boundary={0}'.format(boundary)
         header = {'Content-type': content_type,
                   'Content-length': str(len(body))}
         response = self.http.request(self.api_url + 'requests/',
@@ -160,7 +168,7 @@ class XmlRpcAPIServer(object):
         '''
         Deletes a translation request.
         '''
-        url = self.api_url + 'requests/{}/?token={}'.format(
+        url = self.api_url + 'requests/{0}/?token={1}'.format(
             shortname_or_request_id, token)
         response = self.http.request(url, method='DELETE')
         if response[0].status == 204:
@@ -173,10 +181,10 @@ class XmlRpcAPIServer(object):
         Queries the server for translation results.
         '''
         if shortname_or_request_id is not None:
-            url = self.api_url + 'results/{}/?token={}'.format(
+            url = self.api_url + 'results/{0}/?token={1}'.format(
                 shortname_or_request_id, token)
         else:
-            url = self.api_url + 'results/?token={}'.format(token)
+            url = self.api_url + 'results/?token={0}'.format(token)
         response = self.http.request(url, method='GET')
         if response[0].status == 200:
             return json.loads(response[1])
