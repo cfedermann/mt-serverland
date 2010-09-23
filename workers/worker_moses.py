9c18c7a87dd82ed72d8474ce4e1f7e2546a04e78
Christian Federmann <cfedermann@gmail.com>
2010-09-23 11:31:27 +0200

    Updated worker server implementation to support additional parameters on a per worker basis. This also required changes to the start_worker script.

--- workers/worker_moses.py
+++ workers/worker_moses.py
@@ -8,22 +8,65 @@ from workers.worker import AbstractWorkerServer
 from protobuf.TranslationRequestMessage_pb2 import TranslationRequestMessage
 
 
-# Current settings just pass through the input, adapt to your Moses setup!
-MOSES_CMD = "echo"
-MOSES_CONFIG = ";cat"
-
 class MosesWorker(AbstractWorkerServer):
     """
     Implementation of a worker server that starts a Moses SMT system.
     """
     __name__ = 'MosesWorker'
+    MOSES_CMD = None
+    MOSES_CONFIG = None
+    MOSES_SOURCE = None
+    MOSES_TARGET = None
+    
+    @staticmethod
+    def usage():
+        """
+        Returns usage information, e.g. for additional parameters, etc.
+        """
+        return ('MOSES_CMD=/path/to/moses/binary',
+          'MOSES_CONFIG=/path/to/moses/config>',
+          'MOSES_SOURCE=source_language_iso639_2_code',
+          'MOSES_TARGET=target_language_iso639_2_code')
+    
+    def parse_args(self, args):
+        """
+        Parses the given args list and sets worker specific paramters.
+        """
+        for arg in args:
+            try:
+                key, value = arg.split('=')
+            
+            except ValueError:
+                continue
+            
+            if key == 'MOSES_CMD':
+                print "Setting MOSES_CMD={0}".format(value)
+                self.MOSES_CMD = value
+
+            elif key == 'MOSES_CONFIG':
+                print "Setting MOSES_CONFIG={0}".format(value)
+                self.MOSES_CONFIG = value
+
+            elif key == 'MOSES_SOURCE':
+                print "Setting MOSES_SOURCE={0}".format(value)
+                self.MOSES_SOURCE = value
+
+            elif key == 'MOSES_TARGET':
+                print "Setting MOSES_TARGET={0}".format(value)
+                self.MOSES_TARGET = value
+        
+        if not self.MOSES_CMD or not self.MOSES_CONFIG or \
+          not self.MOSES_SOURCE or not self.MOSES_TARGET:
+            return False
+        
+        return True
 
     def language_pairs(self):
         """
         Returns a tuple of all supported language pairs for this worker.
         """
         return (
-          ('ger', 'eng'), # TODO: Make the output dependent on actual config.
+          (MOSES_SOURCE, MOSES_TARGET),
         )
 
     def language_code(self, iso639_2_code):
