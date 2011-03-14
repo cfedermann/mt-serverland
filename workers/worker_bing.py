3667c3bdd50043cda5ea9605afe8900e0962f1a8
cfedermann <cfedermann@gmail.com>
2011-03-14 21:43:46 +0100

    Updated Bing worker to use TranslateArray via HTTP POST.

--- workers/worker_bing.py
+++ workers/worker_bing.py
@@ -16,6 +16,7 @@ class BingWorker(AbstractWorkerServer):
     """
     __name__ = 'BingWorker'
     __splitter__ = '[BING_SPLITTER_TOKEN]'
+    __batch__ = 200
 
     def language_pairs(self):
         """
@@ -45,41 +46,31 @@ class BingWorker(AbstractWorkerServer):
         return mapping.get(iso639_2_code)
 
 
-    def handle_translation(self, request_id):
-        """
-        Translates text from German->English using Microsoft Translator.
-
-        Requires a Bing AppID as documented at MSDN:
-        - http://msdn.microsoft.com/en-us/library/ff512421.aspx
-        """
-        handle = open('/tmp/{0}.message'.format(request_id), 'r+b')
-        message = TranslationRequestMessage()
-        message.ParseFromString(handle.read())
-
-        source = self.language_code(message.source_language)
-        target = self.language_code(message.target_language)
-
-        # Insert splitter tokens to allow re-construction of original lines.
-        _source_text = []
-        for source_line in message.source_text.split('\n'):
-            _source_text.append(source_line.strip().encode('utf-8'))
-            _source_text.append(self.__splitter__)
-
-        app_id = '9259D297CB9F67680C259FD62734B07C0D528312'
-        the_data = urllib.urlencode({'appId': app_id, 'from': source,
-          'to': target, 'text': u'\n'.join(_source_text)})
+    def _batch_translate(self, source, target, text):
+        """Translates a text using Microsoft Translator."""
+        _text = unicode(text).encode('utf-8')
+        app_id = '9259D297CB9F67680C259FD62734B07C0D528312'        
+        the_xml = """<?xml version="1.0" encoding="utf-8"?>
+<TranslateArrayRequest>
+<AppId>{0}</AppId>
+<From>{1}</From>
+<Texts>
+<string xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays">{2}</string>
+</Texts>
+<To>{3}</To>
+</TranslateArrayRequest>""".format(app_id, source, _text, target)
         the_url = 'http://api.microsofttranslator.com/v2/Http.svc/' \
-          'Translate?{0}'.format(the_data)
-        the_header = {'User-agent': 'Mozilla/5.0'}
+          'TranslateArray'
+        the_header = {'User-agent': 'Mozilla/5.0', 'Content-Type': 'text/xml'}
 
         opener = urllib2.build_opener(urllib2.HTTPHandler)
-        http_request = urllib2.Request(the_url, None, the_header)
+        http_request = urllib2.Request(the_url, the_xml, the_header)
         http_handle = opener.open(http_request)
         content = http_handle.read()
         http_handle.close()
 
-        result_exp = re.compile('<string xmlns="http://schemas.microsoft.' \
-          'com/2003/10/Serialization/">(.*?)</string>', re.I|re.U|re.S)
+        result_exp = re.compile('<TranslatedText>(.*?)</TranslatedText>',
+          re.I|re.U|re.S)
 
         result = result_exp.search(content)
 
@@ -97,14 +88,51 @@ class BingWorker(AbstractWorkerServer):
                     _target_text.append(u' '.join(_current_line))
                     _current_line = []
 
-            message.target_text = u'\n'.join(_target_text)
-            handle.seek(0)
-            handle.write(message.SerializeToString())
+            return u'\n'.join(_target_text)
 
         else:
-            message.target_text = "ERROR: result_exp did not match.\n" \
-              "CONTENT: {0}".format(content)
-            handle.seek(0)
-            handle.write(message.SerializeToString())
+            return "ERROR: result_exp did not match.\nCONTENT: {0}".format(
+              content)
 
+
+    def handle_translation(self, request_id):
+        """"
+        Handler connecting to the Microsoft Translator service.
+
+        Requires a Bing AppID as documented at MSDN:
+        - http://msdn.microsoft.com/en-us/library/ff512421.aspx
+        """
+        handle = open('/tmp/{0}.message'.format(request_id), 'r+b')
+        message = TranslationRequestMessage()
+        message.ParseFromString(handle.read())
+
+        source = self.language_code(message.source_language)
+        target = self.language_code(message.target_language)
+
+        # Insert splitter tokens to allow re-construction of original lines.
+        _source_text = []
+        for source_line in message.source_text.split('\n'):
+            _source_text.append(source_line.strip())
+            _source_text.append(self.__splitter__)
+
+        result = u''
+        batches = len(_source_text) / self.__batch__
+        for batch in range(batches):
+            _start = batch * self.__batch__
+            _end = _start + self.__batch__
+            text = unicode(u'\n'.join(_source_text[_start:_end]))
+            result += self._batch_translate(source, target, text)
+            result += '\n'
+        
+        last_batch = len(_source_text) % self.__batch__
+        if last_batch:
+            print last_batch
+            text = unicode(u'\n'.join(_source_text[-last_batch:]))
+            print text
+            result += self._batch_translate(source, target, text)
+            result += '\n'
+
+        message.target_text = result
+        handle.seek(0)
+        handle.write(message.SerializeToString())
         handle.close()
