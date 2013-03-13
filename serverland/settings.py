8a7b140e4b09ac8d8be372c3b945db296587fbfa
Christian Federmann <cfedermann@dfki.de>
2013-03-13 11:44:02 +0100

    Updated settings.py to work with Django 1.4+

--- serverland/settings.py
+++ serverland/settings.py
@@ -35,8 +35,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = 'development.db'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '{}/development.db'.format(ROOT_PATH),
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -74,9 +78,9 @@ SECRET_KEY = 'fw341l^lu!z7^@&9idq+7$a+b-l*ph_4^1#aj+%+o556fdsu3!'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
