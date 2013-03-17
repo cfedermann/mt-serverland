d6268efcd3f13ee87f79b20a8e6c958d1632b25d
Christian Federmann <cfedermann@gmail.com>
2013-03-17 14:39:33 +0100

    Started using URL reversal in page templates.

--- serverland/dashboard/urls.py
+++ serverland/dashboard/urls.py
@@ -2,13 +2,14 @@
 Project: MT Server Land
  Author: Christian Federmann <cfedermann@dfki.de>
 """
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('serverland.dashboard.views',
-  (r'^$', 'dashboard'),
-  (r'^create/$', 'create'),
-  (r'^delete/(?P<request_id>[a-f0-9]{32})/$', 'delete'),
-  (r'^result/(?P<request_id>[a-f0-9]{32})/$', 'result'),
-  (r'^download/(?P<request_id>[a-f0-9]{32})/$', 'download'),
-  (r'^api/', include('serverland.dashboard.api.urls')),
+  url(r'^$', 'dashboard', name='dashboard'),
+  url(r'^create/$', 'create', name='create'),
+  url(r'^delete/(?P<request_id>[a-f0-9]{32})/$', 'delete', name='delete'),
+  url(r'^result/(?P<request_id>[a-f0-9]{32})/$', 'result', name='result'),
+  url(r'^download/(?P<request_id>[a-f0-9]{32})/$', 'download',
+    name='download'),
+  url(r'^api/', include('serverland.dashboard.api.urls')),
 )
--- serverland/urls.py
+++ serverland/urls.py
@@ -2,7 +2,7 @@
 Project: MT Server Land
  Author: Christian Federmann <cfedermann@dfki.de>
 """
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from serverland.settings import DEBUG, MEDIA_ROOT, DEPLOYMENT_PREFIX
 
@@ -15,13 +15,17 @@ admin.autodiscover()
 PREFIX = DEPLOYMENT_PREFIX[1:]
 
 urlpatterns = patterns('',
-  (r'^{0}/$'.format(PREFIX), 'serverland.views.frontpage'),
-  (r'^{0}/login/$'.format(PREFIX), 'serverland.views.login'),
-  (r'^{0}/logout/$'.format(PREFIX), 'serverland.views.logout',
-    {'next_page': '{0}/'.format(DEPLOYMENT_PREFIX)}),
+  url(r'^{0}/$'.format(PREFIX), 'serverland.views.frontpage',
+    name='frontpage'),
+  url(r'^{0}/login/$'.format(PREFIX), 'serverland.views.login', name='login'),
+  url(r'^{0}/logout/$'.format(PREFIX), 'serverland.views.logout',
+    {'next_page': '{0}/'.format(DEPLOYMENT_PREFIX)}, name='logout'),
   
-  (r'^{0}/dashboard/'.format(PREFIX), include('serverland.dashboard.urls')),
+  url(r'^{0}/dashboard/'.format(PREFIX),
+    include('serverland.dashboard.urls')),
   
+  # TODO: do we actually need this anymore?  We have to move to static files
+  #   anyway, so this seems superfluous at the moment...
   (r'^{0}/site_media/(?P<path>.*)$'.format(PREFIX),
     'django.views.static.serve', {'document_root': MEDIA_ROOT}),
   
